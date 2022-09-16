724354e496dfcb478c778b752fb328543947cd51
sheilawangari <99507318+sheilawangari@users.noreply.github.com>
2022-09-16 23:04:10 +0300

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install the puppent-lint with gem
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# install `flask` version `2.1.0` from `pip3`
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
+

3958a01a27262326a52357d7f2da2ecd8a99dc6b
sheilawangari <99507318+sheilawangari@users.noreply.github.com>
2022-09-16 23:02:41 +0300

    Using Puppet, create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# Create a file named holberton in tmp with owner and group www-data
-file {'/tmp/holberton':
+# create file in `/tmp`
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
-  mode    => '0744',
+  content => 'I love Puppet'
 }
